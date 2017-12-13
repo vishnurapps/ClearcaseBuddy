849a12839f904f84243083d14f73518b9d8b9c9a
Vishnu R <vishnurapps@gmail.com>
2017-12-13 21:51:40 +0530

    Added changes from the first successful part

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,26 +1,73 @@
 #include "mainwindow.h"
+#include "ui_mainwindow.h"
 
-#include <QCoreApplication>
+#include "mainwindow.h"
+#include <QApplication>
 
-MainWindow::MainWindow(QWidget *parent)
-   : QMainWindow(parent)
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
-   // Create the button, make "this" the parent
-   m_button = new QPushButton("My Button", this);
-   // set size and location of the button
-   m_button->setGeometry(QRect(QPoint(100, 100),
-   QSize(200, 50)));
-   lbl_username = new QLabel("Username :", this);
-   //le_username = new QLineEdit(null, this);
 
-   // Connect button signal to appropriate slot
-   connect(m_button, SIGNAL (released()), this, SLOT (handleButton()));
+    ui->setupUi(this);
+    showLoginScreen();
+
 }
 
-void MainWindow::handleButton()
+void MainWindow :: showLoginScreen()
 {
-   // change the text
-   m_button->setText("Example");
-   // resize button
-   m_button->resize(100,100);
+    QWidget *wid = new QWidget(this);
+    QGridLayout *gridLayout = new QGridLayout;
+
+    QLabel *lblMachineIP = new QLabel(this);
+    lblMachineIP->setText("Machine IP");
+    gridLayout->addWidget(lblMachineIP, 0, 0, 1, 1);
+
+    QLabel *lblUsername = new QLabel(this);
+    lblUsername->setText("Username");
+
+    gridLayout->addWidget(lblUsername, 1, 0, 1, 1);
+
+    QLabel *lblPassword = new QLabel(this);
+    lblPassword->setText("Password");
+    gridLayout->addWidget(lblPassword, 2, 0, 1, 1);
+
+    QLineEdit *leMachineIP = new QLineEdit(this);
+    gridLayout->addWidget(leMachineIP, 0, 1, 1, 1);
+
+    QLineEdit *leUsername = new QLineEdit(this);
+    gridLayout->addWidget(leUsername, 1, 1, 1, 1);
+
+    QLineEdit *lePassword = new QLineEdit(this);
+    lePassword->setEchoMode(QLineEdit::Password);
+    gridLayout->addWidget(lePassword, 2, 1, 1, 1);
+
+    QCheckBox *cbRemember = new QCheckBox(this);
+    cbRemember->setChecked(false);
+    cbRemember->setText("Save these credentials");
+    gridLayout->addWidget(cbRemember, 3, 0, 1, 2);
+
+    QPushButton *pbConnect = new QPushButton(this);
+    pbConnect->setText("Connect");
+    gridLayout->addWidget(pbConnect, 4, 0, 1, 1);
+
+    QPushButton *pbCancel = new QPushButton(this);
+    pbCancel->setText("Close");
+    gridLayout->addWidget(pbCancel, 4, 1, 1, 1);
+
+    if(cbRemember->isChecked())
+    {
+
+    }
+
+    wid->setLayout(gridLayout);
+    setCentralWidget(wid);
+
+    connect(pbCancel, SIGNAL(clicked(bool)), this, SLOT(close()));
+    connect(pbConnect, SIGNAL(clicked(bool)), this, SLOT(close()));
+}
+MainWindow::~MainWindow()
+{
+    delete ui;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,25 +3,31 @@
 
 #include <QMainWindow>
 #include <QPushButton>
+#include <QWidget>
 #include <QLabel>
 #include <QLineEdit>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QCheckBox>
+#include <QFile>
+#include <QTextStream>
 
 namespace Ui {
-   class MainWindow;
+class MainWindow;
 }
 
 class MainWindow : public QMainWindow
 {
-   Q_OBJECT
-public:
-   explicit MainWindow(QWidget *parent = 0);
-private slots:
-   void handleButton();
-private:
-   QPushButton *m_button;
-   QLabel *lbl_username;
-   QLineEdit *le_username;
+    Q_OBJECT
 
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    void showLoginScreen();
 };
 
 #endif // MAINWINDOW_H
+
